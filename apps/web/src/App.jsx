7e82c44bd59be2eb89460706ce990ffd1f862f64
Sandro Alves Leão <sandro.alves.leao@gmail.com>
2026-04-01 17:48:34 -0300

    feat: atualiza link do whatsapp e configuracao da vercel com analytics

--- apps/web/src/App.jsx
+++ apps/web/src/App.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
 import ScrollToTop from './components/ScrollToTop.jsx';
 import HomePage from './pages/HomePage.jsx';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
       </Routes>
+      <Analytics />
     </Router>
   );
 }
